73ec4fb55b4e7719b2cd2dfb8645d452377e948f
vegasten <vegard@kodeworks.no>
2022-04-23 23:19:36 +0200

    Animate friendly monster

--- Assets/BattleManager.cs
+++ Assets/BattleManager.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
+    [Header("Temporary monster")]
+    [SerializeField] private BattleMonster _friendlyBattleMonster;
+    [SerializeField] private BattleMonster _enemyBattleMonster;
+
+    [Header("Battle Presenter")]
     [SerializeField] private BattlePresenter _presenter;
 
-    private PlayerData _player;
-    private PlayerData _enemy;
+    private PlayerData _friendlyMonsterData;
+    private PlayerData _enemyMonsterData;
 
     private void Start()
     {
@@ -14,8 +20,8 @@ public class BattleManager : MonoBehaviour
 
     public void StartBattle()
     {
-        _player = new PlayerData(100);
-        _enemy = new PlayerData(100);
+        _friendlyMonsterData = new PlayerData(100);
+        _enemyMonsterData = new PlayerData(100);
 
         _presenter.SetHealthBar(1, Players.Player);
         _presenter.SetHealthBar(1, Players.Enemy);
@@ -24,11 +30,20 @@ public class BattleManager : MonoBehaviour
     public void RegisterComboResult(int numberOfCombos, int numberOfTiles)
     {
         int damage = CalculateDamage(numberOfCombos, numberOfTiles);
-        _enemy.TakeDamage(damage);
-        _presenter.SetHealthBar(_enemy.GetPercentLife(), Players.Enemy);
+        _enemyMonsterData.TakeDamage(damage);
+
+        if (numberOfCombos <= 2)
+        {
+            StartCoroutine(AnimateFriendlyAttack());
+        }
+        else
+        {
+            StartCoroutine(AnimateFriendlyBigAttack());
+        }
+
         Debug.Log($"Doing {damage} points of damage to the enemy");
 
-        if (_enemy.Life <= 0)
+        if (_enemyMonsterData.Life <= 0)
         {
             Debug.Log("Enemy is dead!");
             _presenter.SetVictoryText();
@@ -48,5 +63,41 @@ public class BattleManager : MonoBehaviour
     private int CalculateDamage(int numberOfCombos, int numberOfTiles)
     {
         return numberOfCombos * numberOfTiles;
+    }
+
+    private IEnumerator AnimateFriendlyAttack()
+    {
+        _friendlyBattleMonster.TriggerAttack();
+        yield return new WaitForSeconds(0.2f);
+        _enemyBattleMonster.TriggerHit();
+        yield return new WaitForSeconds(0.5f);
+        _presenter.SetHealthBar(_enemyMonsterData.GetPercentLife(), Players.Enemy);
+    }
+
+    private IEnumerator AnimateFriendlyBigAttack()
+    {
+        _friendlyBattleMonster.TriggerBigAttack();
+        yield return new WaitForSeconds(0.5f);
+        _enemyBattleMonster.TriggerHit();
+        yield return new WaitForSeconds(0.5f);
+        _presenter.SetHealthBar(_enemyMonsterData.GetPercentLife(), Players.Enemy);
+    }
+
+    private IEnumerator AnimateEnemyAttack()
+    {
+        _enemyBattleMonster.TriggerAttack();
+        yield return new WaitForSeconds(0.2f);
+        _friendlyBattleMonster.TriggerHit();
+        yield return new WaitForSeconds(0.5f);
+        _presenter.SetHealthBar(_friendlyMonsterData.GetPercentLife(), Players.Enemy);
+    }
+
+    private IEnumerator AnimateEnemyBigAttack()
+    {
+        _enemyBattleMonster.TriggerBigAttack();
+        yield return new WaitForSeconds(0.5f);
+        _friendlyBattleMonster.TriggerHit();
+        yield return new WaitForSeconds(0.5f);
+        _presenter.SetHealthBar(_friendlyMonsterData.GetPercentLife(), Players.Enemy);
     }
 }
